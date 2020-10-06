feb32ef9d00e6f7c653cde823c6765b57770ecdb
DaemonSharps <56834210+DaemonSharps@users.noreply.github.com>
2020-10-06 19:47:58 +0300

    delete

--- lincs.sql
+++
@@ -1,8 +0,0 @@
-use products;
-create table lincs
-( categ_ID int,
-  prod_ID int);
-  insert lincs(categ_ID,prod_ID)
-  values (1,6),(1,7),(1,8),(1,9),(1,10),(1,11),(2,1),(2,2),(2,3),(2,4),
-  (3,6),(3,7),(3,8),(4,12),(4,13),(null,14),(null,15);
-  
