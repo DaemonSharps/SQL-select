5eae41110813f23badea816ad34df0b84fa0efb8
DaemonSharps <56834210+DaemonSharps@users.noreply.github.com>
2020-10-06 19:48:13 +0300

    delete

--- products.sql
+++
@@ -1,9 +0,0 @@
-use products;
-create table products1
-(Id int primary key auto_increment,
-namePD varchar(50) unique not null
-);
-insert products1(namePD)
-value ('meat'),('juise'),('apple'),('banana'),('milk'),('soap'),('scrab'),
-('shampoo'),('vanish'),
-('fary'),('Mr.Muscle'),('broom'),('bucket'),('car'),('plane');
