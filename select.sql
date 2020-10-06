df9d274db5007baeb12b6b1fc32ae6cf1c8d9481
DaemonSharps <56834210+DaemonSharps@users.noreply.github.com>
2020-10-06 19:48:28 +0300

    delete script

--- select.sql
+++
@@ -1,6 +0,0 @@
-use products;
-select products1.namePD,categories.NameCT
-from lincs 
- join products1 on products1.ID=lincs.prod_ID
-left join categories on categories.ID=lincs.categ_ID
-order by products1.namePD;
